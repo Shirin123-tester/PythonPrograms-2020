00937f7fa528a77f0cd9ce3fbcff88f44ccffe67
Shirin123-tester <bhatshirin@gmail.com>
2020-01-23 22:20:26 +0530

    Math operations using function

---
+++ Mathoperation.py
@@ -0,0 +1,21 @@
+"Program to demontrsate functions "
+
+def add():
+    a=10
+    b=20
+    sum=a+b
+    print("Addition = ",sum)
+
+def substrsct(no1,no2):
+    print("Substraction =",no1-no2)
+
+def multiplication(s1,s2=10):
+    print("Multiplication = ",s1*s2)
+
+def devision(d1,d2):
+    return (d1/d2)
+
+add()
+substrsct(56,30)
+multiplication(5)
+print(devision(40,5))
