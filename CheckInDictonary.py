dd08d7a5c68888dccc6a2e41aa3d59c4d41f82bd
Shirin123-tester <bhatshirin@gmail.com>
2020-01-23 21:38:30 +0530

    Dictionary exercise

---
+++ CheckInDictonary.py
@@ -0,0 +1,9 @@
+" Program to check if given key is present in dictionary or not"
+mydictionary={"red","green","blue","yellow","orange"}
+colour=input("Please enter a colour to check in dictionary ")
+if colour in mydictionary:
+    print("colour present in dictonary")
+
+else:
+    print("Colour is not present in dictionary")
+    
