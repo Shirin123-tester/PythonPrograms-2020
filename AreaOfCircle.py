86721f6384505b1a932b22d131fa8e91cd3c1075
Shirin123-tester <bhatshirin@gmail.com>
2020-01-23 21:53:38 +0530

    Area Of Circle

---
+++ AreaOfCircle.py
@@ -0,0 +1,6 @@
+'Program to clculate Aea of circle'
+redius=int(input('Please enter redius of circle to calculate area '))
+pi=3.14
+area=pi*redius*redius
+print("redius of cilcle =",redius)
+print("Area of Circle = ",area)
