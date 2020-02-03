981f121c61c1071f85325909a1b8619f9e12e136
Shirin123-tester <bhatshirin@gmail.com>
2020-02-03 15:25:19 +0530

    Program to swap two numbers

---
+++ SwapTwoVeriables.py
@@ -0,0 +1,13 @@
+" Program to swap two veriables using third variable "
+number1=int(input("Please enter first number "))
+number2=int(input("Please enter second number "))
+
+print("First number = ",number1)
+print("second number = ",number2)
+
+number3=number1
+number1=number2
+number2=number3
+
+print("Number1 after swap = ",number1)
+print("number2 after swap = ",number2)
