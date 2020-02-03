c107fbf53224baf3b2a1e6fce1efc470f81301f9
Shirin123-tester <bhatshirin@gmail.com>
2020-02-03 15:24:42 +0530

    Program to check if number is -Ve,+ve or Zero

---
+++ CheckNumber.py
@@ -0,0 +1,10 @@
+"Program to check weather the number is +ve,-ve or zero "
+
+number=int(input("Please enter any number "))
+
+if number==0:
+    print(number,"is zero ")
+elif number<0:
+    print(number,"is negative number")
+else:
+    print(number,"is positive number")
